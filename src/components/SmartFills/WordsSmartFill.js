9d6b874e542006389401d818031aa09959f25615
ketikketik <aazhard8@gmail.com>
2022-08-31 16:20:46 +0700

    Add choose word feature in WordsSmartFill to fill InvoiceForm

--- src/components/SmartFills/WordsSmartFill.js
+++ src/components/SmartFills/WordsSmartFill.js
@@ -1,7 +1,71 @@
-import React from 'react'
-import { Alert, Button, Card, Spinner } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Alert, Button, Card, Spinner, Toast, ToastContainer } from 'react-bootstrap';
+import moment from 'moment'
 
-const WordsSmartFill = ({ words, isLoading, invoiceFormInputFocus }) => {
+const WordsSmartFill = ({ words, isLoading, invoiceFormInputFocus, invoicePayload, setInvoicePayload }) => {
+	const [showErrorToast, setShowErrorToast] = useState(false)
+	const [errorToastMsg, setErrorToastMsg] = useState("")
+
+	const onWordButtonClick = (word) => {
+		return () => {
+			if (!isLoading && invoiceFormInputFocus && word) {
+				if (invoiceFormInputFocus === "InvoiceNumber") {
+					setInvoicePayload({ ...invoicePayload, invoiceNumber: word })
+				}
+				else if (invoiceFormInputFocus === "InvoiceDate") {
+					const isValidInvoiceDate = moment(word).isValid();
+					if (isValidInvoiceDate) {
+						const invoiceDate = moment(word).format("YYYY-MM-DD")
+						setInvoicePayload({ ...invoicePayload, invoiceDate: invoiceDate })
+					} else {
+						setErrorToastMsg(`"${word}" isn't a valid date format`)
+						setShowErrorToast(true)
+					}
+				}
+				else if (invoiceFormInputFocus === "DueDate") {
+					const isValidDueDate = moment(word).isValid();
+					if (isValidDueDate) {
+						const dueDate = moment(word).format("YYYY-MM-DD")
+						setInvoicePayload({ ...invoicePayload, dueDate: dueDate })
+					} else {
+						setErrorToastMsg(`"${word}" isn't a valid date format`)
+						setShowErrorToast(true)
+					}
+				}
+				else if (invoiceFormInputFocus.includes("ProductDescription")) {
+					const productIndex = parseInt(invoiceFormInputFocus.charAt(invoiceFormInputFocus.length - 1))
+					invoicePayload.products[productIndex].description = word;
+					setInvoicePayload({ ...invoicePayload })
+				}
+				else if (invoiceFormInputFocus.includes("ProductQuantity")) {
+					const isValidProductQuantity = !isNaN(parseInt(word));
+					if (isValidProductQuantity) {
+						const productIndex = parseInt(invoiceFormInputFocus.charAt(invoiceFormInputFocus.length - 1))
+						invoicePayload.products[productIndex].quantity = parseInt(word);
+						setInvoicePayload({ ...invoicePayload })
+					} else {
+						setErrorToastMsg(`"${word}" isn't a valid number format`)
+						setShowErrorToast(true)
+					}
+				}
+				else if (invoiceFormInputFocus.includes("ProductPrice")) {
+					const isValidProductPrice = !isNaN(parseInt(word));
+					if (isValidProductPrice) {
+						const productIndex = parseInt(invoiceFormInputFocus.charAt(invoiceFormInputFocus.length - 1))
+						invoicePayload.products[productIndex].quantity = parseInt(word);
+						setInvoicePayload({ ...invoicePayload })
+					} else {
+						setErrorToastMsg(`"${word}" isn't a valid number format`)
+						setShowErrorToast(true)
+					}
+				}
+			}
+		}
+	}
+
+	const onErrorToastButtonClick = () => {
+		setShowErrorToast(false);
+	}
 
 	const FocusAlert = () => {
 		if (!isLoading && invoiceFormInputFocus && words.length) {
@@ -29,18 +93,33 @@ const WordsSmartFill = ({ words, isLoading, invoiceFormInputFocus }) => {
 	}
 
 	return (
-		<Card>
-			<Card.Header>Words SmartFill</Card.Header>
-			<Card.Body>
-				{isLoading && <div className='d-flex justify-content-center m-3'><Spinner animation="border" variant="primary" size="lg" /></div>}
-				<FocusAlert />
-				{!isLoading && words.map(elem => {
-					return (
-						<Button style={{ margin: "4px" }}>{elem.text}</Button>
-					)
-				})}
-			</Card.Body>
-		</Card>
+		<>
+			<Card>
+				<Card.Header>Words SmartFill</Card.Header>
+				<Card.Body>
+					{isLoading && <div className='d-flex justify-content-center m-3'><Spinner animation="border" variant="primary" size="lg" /></div>}
+					<FocusAlert />
+					{!isLoading && words.map((elem, index) => {
+						return (
+							<Button key={index} style={{ margin: "4px" }} onClick={onWordButtonClick(elem.text)}>{elem.text}</Button>
+						)
+					})}
+				</Card.Body>
+			</Card>
+			<ToastContainer className="p-3" position="top-end">
+				<Toast
+					onClose={onErrorToastButtonClick}
+					show={showErrorToast}
+					delay={3000}
+					autohide
+				>
+					<Toast.Header>
+						<strong className="me-auto">Error</strong>
+					</Toast.Header>
+					<Toast.Body>{errorToastMsg}</Toast.Body>
+				</Toast>
+			</ToastContainer>
+		</>
 	)
 }
 
