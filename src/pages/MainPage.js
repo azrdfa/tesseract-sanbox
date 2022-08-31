2aca09da3dd6036f853402b1a1a3596a2e2d229c
ketikketik <aazhard8@gmail.com>
2022-08-31 13:13:36 +0700

    Move state management to MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Stack } from 'react-bootstrap'
-import { DetectionForm, InvoiceForm } from '../components'
+import { DetectionForm, InvoiceForm, WordsSmartFill } from '../components'
 
 const MainPage = () => {
+  const [data, setData] = useState({
+    words: []
+  })
+  const [isLoading, setIsLoading] = useState(false)
+  const [invoiceFormInputFocus, setInvoiceFormInputFocus] = useState("");
+
+  const setDataWords = (words) => {
+    setData({ ...data, words })
+  }
+
   return (
     <Container fluid>
       <br />
       <Row>
         <Col sm={12} md={6} lg={4}>
           <Stack gap={3}>
-            <DetectionForm />
-            <InvoiceForm />
+          <DetectionForm
+              setDataWords={setDataWords}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading} />
+            <InvoiceForm setInvoiceFormInputFocus={setInvoiceFormInputFocus} />
           </Stack>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <WordsSmartFill
+            words={data.words}
+            isLoading={isLoading}
+            invoiceFormInputFocus={invoiceFormInputFocus}
+          />
         </Col>
       </Row>
       <br />
